3c822d437a860b3a2d113fa55c429df5bcdab4f8
SaiyamJain <saiyamjain3000@gmail.com>
2022-01-16 20:53:13 +0530

    Added doctorDecorator

--- flaskproject/__init__.py
+++ flaskproject/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+
 
 app = Flask(__name__)
-CORS(app)
+
 app.secret_key = "mysecretkey123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Admin@localhost/medico"
 app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
@@ -11,5 +11,6 @@ db = SQLAlchemy(app)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+
 
 from flaskproject import routes
